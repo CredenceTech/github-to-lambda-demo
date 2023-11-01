0fa3af3beb20419c1ae728ae0e5af9c1ddcad921
MayankMehta79 <mayank.credencetech@gmail.com>
2023-11-01 16:11:35 +0530

    Update Condition

--- index.mjs
+++ index.mjs
@@ -1,10 +1,9 @@
-export const handler = async (event) => {
-    // TODO implement
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify('Hello from Lambda! Testing'),
-    };
-    return response;
-  };
+console.log('Loading function');
 
-  
+export const handler = async (event) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    for (const { messageId, body } of event.Records) {
+        console.log('SQS message %s: %j', messageId, body);
+    }
+    return `Successfully processed ${event.Records.length} messages.`;
+};
