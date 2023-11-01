eb339b76576f364f2d779c9e345f8a28a19c3400
MayankMehta79 <mayank.credencetech@gmail.com>
2023-11-01 17:35:42 +0530

    Update Condition

--- index.mjs
+++ index.mjs
@@ -4,6 +4,7 @@ export const handler = async (event) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
     for (const { messageId, body } of event.Records) {
         console.log('SQS message %s: %j', messageId, body);
+        console.log('Testing');
     }
     return `Successfully processed ${event.Records.length} messages.`;
 };
