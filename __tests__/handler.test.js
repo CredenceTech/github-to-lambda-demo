9ec8a9c06ecb39bd71d3174e3d1398a06302f542
MayankMehta79 <mayank.credencetech@gmail.com>
2023-11-02 16:09:13 +0530

    Update

--- __tests__/handler.test.js
+++ __tests__/handler.test.js
@@ -1,5 +1,29 @@
-const handler = require('../handler');
- 
+const { handler } = require('../index');
+
+// Example event object (simulating an SQS event)
+const sampleEvent = {
+  Records: [
+    {
+      messageId: '1',
+      body: 'Sample message 1',
+    },
+    {
+      messageId: '2',
+      body: 'Sample message 2',
+    },
+  ],
+};
+
+
 test('correct greeting is generated', () => {
-  expect(handler.printHelloWorld()).toBe("Hello World");
+  expect(handler(sampleEvent)
+    .then((result) => {
+      console.log('Lambda function result:', result);
+    })
+    .catch((error) => {
+      console.error('Lambda function error:', error);
+    }));
 });
+
+// Invoke the Lambda function handler with the sample event
+
---
+++ index.js
@@ -0,0 +1,40 @@
+'use strict';
+
+console.log('Loading function');
+
+module.exports.handler = async (event) => {
+    //console.log('Received event:', JSON.stringify(event, null, 2));
+    for (const { messageId, body } of event.Records) {
+        console.log('SQS message %s: %j', messageId, body);
+        console.log('Testing');
+    }
+    return `Successfully processed ${event.Records.length} messages.`;
+};
+
+// export const handler = async (event) => {
+//     //console.log('Received event:', JSON.stringify(event, null, 2));
+//     for (const { messageId, body } of event.Records) {
+//         console.log('SQS message %s: %j', messageId, body);
+//         console.log('Testing');
+//     }
+//     return `Successfully processed ${event.Records.length} messages.`;
+// };
+
+// module.exports.helloworld = async (event,context,callback) => {
+//   const response = {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: printHelloWorld(),
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+// callback(null,response)
+// };
+// function printHelloWorld(){
+//   return "Hello World";
+// }
+// module.exports.printHelloWorld=printHelloWorld;
